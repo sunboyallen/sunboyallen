90d68ea325993c92cf25803acfe949c39155aed4
Allen-Sunboy <yangnan_sunboy@163.com>
2023-11-23 20:48:49 +0800

    feat: 添加gcd.py

---
+++ gcd.py
@@ -0,0 +1,7 @@
+def gcd(m: int, n: int) -> int:
+    return m if n == 0 else gcd(n, m % n)
+
+if __name__ == '__main__':
+    m = int(input('请输入第一个整数: '))
+    n = int(input('请输入第二个整数: '))
+    print('最大公约数为: ', gcd(m, n))
