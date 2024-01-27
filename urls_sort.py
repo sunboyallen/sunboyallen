79e99431dd17e7ede88156d9f03b2ee81ceccaec
Allen-Sunboy <yangnan_sunboy@163.com>
2024-01-27 11:47:28 +0800

    urls_sort

--- urls_sort.py
+++ urls_sort.py
@@ -1,58 +1,58 @@
 import tldextract
 from urllib.parse import urlparse
 
-tld_extract = tldextract.TLDExtract(cache_dir=False)
-
-urls = []
-
-with open('urls.md', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-    for i in range(len(lines)):
-        line = lines[i].strip()
-
-        if line and not line.startswith('#') and not line.startswith('<!'):
-            url = {'url': line}
-            if i < len(lines) and lines[i + 1].startswith('<!'):
-                url['comment'] = lines[i + 1]
-
-            urls.append(url)
+tld_extract = tldextract.TLDExtract(cache_dir=None)
 
 
 def custom_sort_key(url_info: dict):
     url: str = url_info['url']
     tld_info = tld_extract(url)
-    path: str = urlparse(url).path
+    path = urlparse(url).path
     subdomain_key = tld_info.subdomain
-
     if tld_info.subdomain == '' or tld_info.subdomain == 'www':
         subdomain_key = '0'
-
     return (tld_info.domain, tld_info.suffix, subdomain_key, path)
 
 
-urls.sort(key=custom_sort_key)
+def urls_sort(filepath: str):
+    urls = []
+    with open(filepath, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        for i in range(len(lines)):
+            line = lines[i].strip()
+            if line and not line.startswith('#') and not line.startswith('<!'):
+                url = {'url': line}
+                if i < len(lines) and lines[i + 1].startswith('<!'):
+                    url['comment'] = lines[i + 1]
 
-with open('urls.md', 'w', encoding='utf-8') as f:
-    f.write('# URLs\n')
-    previous_first_letter = None
+                urls.append(url)
 
-    for i in range(len(urls)):
-        tld_info = tld_extract(urls[i]['url'])
-        current_first_letter = tld_info.domain[0] if tld_info.domain else None
+    urls.sort(key=custom_sort_key)
 
-        if current_first_letter != previous_first_letter:
-            f.write('\n')
-            f.write('## ' + current_first_letter + '\n\n')
+    with open(filepath, 'w', encoding='utf-8') as f:
+        f.write('# URLs\n')
+        for i in range(len(urls)):
+            current_tld_info = tld_extract(urls[i]['url'])
+            current_first_letter = current_tld_info.domain[0]
+            previous_tld_info = None
+            if i > 0:
+                previous_tld_info = tld_extract(urls[i - 1]['url'])
+            previous_first_letter = None
+            if previous_tld_info:
+                previous_first_letter = previous_tld_info.domain[0]
 
-        if i > 0:
-            tld_info1 = tld_extract(urls[i - 1]['url'])
-            tld_info2 = tld_extract(urls[i]['url'])
-            if tld_info1.domain != tld_info2.domain and tld_info1.domain[0] == tld_info2.domain[0]:
+            if current_first_letter != previous_first_letter:
+                f.write('\n')
+                f.write('## ' + current_first_letter + '\n\n')
+
+            if current_first_letter == previous_first_letter and current_tld_info.domain != previous_tld_info.domain:
                 f.write('\n')
 
-        f.write(urls[i]['url'] + '\n')
+            f.write(urls[i]['url'] + '\n')
 
-        if 'comment' in urls[i]:
-            f.write(urls[i]['comment'])
+            if 'comment' in urls[i]:
+                f.write(urls[i]['comment'])
 
-        previous_first_letter = current_first_letter
+
+if __name__ == '__main__':
+    urls_sort('urls.md')
